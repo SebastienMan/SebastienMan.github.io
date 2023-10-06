68b9c65650cb200fe16a7ccc61ac77070df87119
SebastienMan <sebastien.manach@oclock.school>
2023-10-06 22:53:43 +0200

    Création des dossier

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../SebastienMan.github.io/css/reset.css" rel="stylesheet">
+    <link href="../SebastienMan.github.io/css/style.css" rel="stylesheet">
+    <title>Curriculum vitae Sébastien Manach</title>
+</head>
+
+<body>
+
+    <header class="header__title__h1h2">
+        <h1>Sébastien MANACH</h1>
+        <h2>Développeur Web</h2>
+        <h2>React & Symfony sur Marseille</h2>
+    </header>
+
+    <section class="section__left__portfolio">
+        <div>
+
+        </div>
+    </section>
+
+    <section class="section__right__cv">
+        <div>
+
+        </div>
+    </section>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
